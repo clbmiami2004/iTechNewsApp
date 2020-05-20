5f843d71d90011d50d2f54cdc51d311b72322c69
Christian Lorenzo <clbmiami2004@gmail.com>
2020-05-20 13:01:52 -0700

    Initial Commit

---
+++ iTechNewsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iTechNewsApp
+//
+//  Created by Christian Lorenzo on 5/20/20.
+//  Copyright © 2020 Christian Lorenzo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
